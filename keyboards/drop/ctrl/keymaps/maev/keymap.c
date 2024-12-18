db15a2f4bb270bbb5ebb622d0f4385359c57c354
Maev Dunlap <maev.dunlap@icloud.com>
2024-12-18 10:07:12 -0500

    added emojii button to drop/ctrl/v2

--- keyboards/drop/ctrl/keymaps/maev/keymap.c
+++ keyboards/drop/ctrl/keymaps/maev/keymap.c
@@ -5,17 +5,16 @@
 enum LAYER_NAMES {
     BASE_LYR,
     SETTINGS_LYR,
-    MOUSE_LYR,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LYR] = LAYOUT_tkl_ansi(
-        KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,              KC_VOLD, KC_MPLY, KC_VOLU,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,             KC_INS,  KC_HOME, KC_PGUP,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,             KC_DEL,  KC_END,  KC_PGDN,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                      KC_UP,
-        DF(MOUSE_LYR), KC_LOPT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_ROPT,  KC_RCTL, MO(SETTINGS_LYR),      KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,              KC_VOLD, KC_MPLY, KC_VOLU,
+        KC_GRV,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,             KC_INS,  KC_HOME, KC_PGUP,
+        KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,             KC_DEL,  KC_END,  KC_PGDN,
+        KC_LCTL, KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,                      KC_UP,
+        LCTL(LCMD(KC_SPC)), KC_LOPT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_ROPT,  KC_RCTL, MO(SETTINGS_LYR),      KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [SETTINGS_LYR] = LAYOUT_tkl_ansi(
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
@@ -25,12 +24,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,QK_BOOT, NK_TOGG, _______, _______, _______, _______,          _______,             _______,
         _______, _______, _______,                            EE_CLR,                             _______, _______, _______, _______,    ROPT(KC_LEFT), _______, ROPT(KC_RIGHT)
     ),
-    [MOUSE_LYR] = LAYOUT_tkl_ansi(
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,             _______,
-        DF(BASE_LYR),   _______, _______,                            _______,                            _______, _______, _______, _______,    _______, _______, _______
-    )
 };
